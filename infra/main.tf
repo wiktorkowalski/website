cc6fb4dc4a0f5fdd0178c7892209bd6e57ba4a1d
wiktorkowalski <mail@wiktorkowalski.pl>
2023-06-22 22:37:04 +0200

    feat: terraform

---
+++ infra/main.tf
@@ -0,0 +1,69 @@
+//terraform cloud
+terraform {
+  cloud {
+    organization = "wiktor9196667"
+    workspaces {
+      name = "website"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.2.0"
+    }
+  }
+}
+
+variable aws_access_key {
+  type = string
+  default = ""
+}
+
+variable "aws_secret_key" {
+  type = string
+  default = ""
+}
+
+variable "github_access_token" {
+  type = string
+  default = ""
+}
+
+// aws
+provider "aws" {
+  region  = "eu-central-1"
+  
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+// import existing aws route 53 zone
+data "aws_route53_zone" "wiktorkowalski" {
+  name = "wiktorkowalski.pl"
+}
+
+//aws amplify app
+resource "aws_amplify_app" "website" {
+  name         = "website"
+  repository   = "https://github.com/wiktorkowalski/website"
+  access_token = var.github_access_token
+
+  build_spec = file("build_spec.yml")
+}
+
+// aws amplify branch
+resource "aws_amplify_branch" "master" {
+  app_id      = aws_amplify_app.website.id
+  branch_name = "master"
+}
+
+// aws amplify domain
+resource "aws_amplify_domain_association" "website" {
+  app_id      = aws_amplify_app.website.id
+  domain_name = "wiktorkowalski.pl"
+  sub_domain {
+    branch_name = aws_amplify_branch.master.branch_name
+    prefix      = ""
+  }
+}
