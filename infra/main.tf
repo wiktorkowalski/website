5e08a70f428c063ba03de1b9ee4a88cf85ed4da1
wiktorkowalski <wiktor9196667@gmail.com>
2024-01-25 18:21:06 +0100

    chore: add custom error response

--- infra/main.tf
+++ infra/main.tf
@@ -113,6 +113,13 @@ resource "aws_cloudfront_distribution" "website_distribution" {
     }
   }
 
+  custom_error_response {
+    error_caching_min_ttl = 0
+    error_code = 403
+    response_code = 200
+    response_page_path = "/index.html"
+  }
+
   enabled             = true
   default_root_object = "index.html"
 
